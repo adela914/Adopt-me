b668fe393808fa187add1766f0b582124f2f97ba
Kyunghwa <adela914@gmail.com>
2020-07-21 16:48:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SearchParams from './SearchParams'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <SearchParams />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
